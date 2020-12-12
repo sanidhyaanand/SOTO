3f175b4584c3147edd158751a47408a9b2eac439
sanidhyaanand <sanidhya.anand03@gmail.com>
2020-12-13 01:54:46 +0530

    Added necessary files and overall structure

--- controller.js
+++ controller.js
@@ -1,5 +1,39 @@
-// Author: Sanidhya Anand 12-12-2020
+// Sanidhya Anand 12-12-2020
 
-const Controller = function(){
+class Controller {
+    constructor(){
+        this.right = new Controller.Input();
+        this.left = new Controller.Input();
+        this.up = new Controller.Input();
+        
+        // Tracks key input
+        this.keyDownUp = function(type, key_code) {
+
+            var down = (type == "keydown") ? true : false;
+        
+            switch(key_code) {
+        
+              case 37: this.left.getInput(down);  break;
+              case 38: this.up.getInput(down);    break;
+              case 39: this.right.getInput(down);
+        
+            }
+        
+        };
+    }
     
+    static Input(){
+        this.active = this.down = false;
+    }
 }
+
+Controller.Input.prototype = {
+
+    constructor: Controller.Input,
+  
+    getInput: function(down) {
+      if (this.down != down) this.active = down;
+      this.down = down;
+    }
+  
+};
--- main.js
+++ main.js
@@ -1,13 +1,49 @@
-// Author: Sanidhya Anand 12-12-2020
+// Sanidhya Anand 12-12-2020
 
 /*  The seperation of our entire logic is done according to the MVC approach  
     This is all I can say */
 
-    // Controller handles user input
+window.addEventListener("load", function(event){
+
+    var keyDownUp = function(event) {
+        controller.keyDownUp(event.type, event.keyCode);
+    };
+
+    var resize = function(event) {
+        display.resize(document.documentElement.clientWidth - 32, document.documentElement.clientHeight - 32, game.world.height / game.world.width);
+        display.render();
+    };
+
+    var render = function() {
+        display.fill(game.world.background_color); // Clear background to game's background color.
+        // Render character
+        display.drawRectangle(game.world.player.x, game.world.player.y, game.world.player.width, game.world.player.height, game.world.player.color);
+        display.render();
+    };
+
+    var update = function() {
+
+        if (controller.left.active)  { game.world.player.moveLeft();  }
+        if (controller.right.active) { game.world.player.moveRight(); }
+        if (controller.up.active)    { game.world.player.jump(); controller.up.active = false; }
+    
+        game.update();
+    
+    };
+    
     var controller = new Controller();
+    var display    = new Display(document.querySelector("canvas"));
+    var game       = new Game();
+    var engine     = new Engine(1000/30, render, update);
 
-    // Display handles window resizing and rendering 
-    var display = new Display();
+    display.buffer.canvas.height = game.world.height;
+    display.buffer.canvas.width = game.world.width;
 
-    // Game handles the game logic
-    var game = new Game();
+    window.addEventListener("keydown", keyDownUp);
+    window.addEventListener("keyup",   keyDownUp);
+    window.addEventListener("resize",  resize);
+
+    resize();
+
+    engine.start();
+});
