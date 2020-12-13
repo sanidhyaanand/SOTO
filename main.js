f7c37785de39890bccccba96e41852b252a7302b
sanidhyaanand <sanidhya.anand03@gmail.com>
2020-12-13 10:29:04 +0530

    First rendering successful

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ window.addEventListener("load", function(event){
 
         if (controller.left.active)  { game.world.player.moveLeft();  }
         if (controller.right.active) { game.world.player.moveRight(); }
-        if (controller.up.active)    { game.world.player.jump(); controller.up.active = false; }
+        if (controller.up.active)    { game.world.player.jump(); controller.up.active = false }
     
         game.update();
     
